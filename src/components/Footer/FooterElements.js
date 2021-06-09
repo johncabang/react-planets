cf912b6289b459fbb06ccabf44b7d4c3d11fc845
John Cabang <johncabz@gmail.com>
2021-06-08 23:33:22 -0400

    Fix Footer styling - tablet view

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -5,6 +5,11 @@ export const StyledFooterContainer = styled.div`
   justify-content: space-between;
   width: 100%;
   margin: 5rem 0;
+
+  @media screen and (max-width: 768px) {
+    justify-content: space-between;
+    margin: 5rem 2rem;
+  }
 `;
 
 export const StyledFooterWrapper = styled.div`
@@ -15,14 +20,27 @@ export const StyledFooterWrapper = styled.div`
   height: 120px;
   border: 1px solid ${(props) => props.theme.colors.neutral.darkGray};
   margin: 0;
+
+  @media screen and (max-width: 768px) {
+    width: 164px;
+    height: 88px;
+  }
 `;
 
 export const Heading = styled.h4`
   color: ${(props) => props.theme.colors.neutral.darkGray};
   text-transform: uppercase;
+
+  @media screen and (max-width: 768px) {
+    font-size: 8px;
+  }
 `;
 
 export const StyledH2 = styled.h2`
   color: ${(props) => props.theme.colors.neutral.white};
   text-transform: uppercase;
+
+  @media screen and (max-width: 768px) {
+    font-size: 24px;
+  }
 `;
